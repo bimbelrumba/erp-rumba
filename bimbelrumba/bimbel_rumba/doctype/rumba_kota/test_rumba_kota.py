36fa9653096acb63c7e0110580185d10385656c3
Bimbel Rumba <yrki@vmi3126847.contaboserver.net>
2026-03-07 09:00:33 +0700

    Add RUMBA Kota doctype

---
+++ bimbelrumba/bimbel_rumba/doctype/rumba_kota/test_rumba_kota.py
@@ -0,0 +1,9 @@
+# Copyright (c) 2026, Rumba Kita Indonesia and Contributors
+# See license.txt
+
+# import frappe
+from frappe.tests.utils import FrappeTestCase
+
+
+class TestRUMBAKota(FrappeTestCase):
+	pass
